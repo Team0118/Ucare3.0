2b7489813153ff7c9a3a76804baf75cdf9b25f38
Vidal Leonardo De Los Santos Rincon <44832449+VidalLeonardoDeLosSantosRincon@users.noreply.github.com>
2018-12-02 19:02:14 -0400

    Correccion de errores

--- Ucare.Web/Startup.cs
+++ Ucare.Web/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Ucare.Datos.Db
+
 
 namespace Ucare.Web
 {
