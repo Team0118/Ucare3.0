2ed704b6ff0fc3d16d0b4fb8f4a72b283fd56aab
CristoferNC <cristofercorporan675@hotmail.com>
2018-12-03 09:38:30 -0400

    .

--- Ucare.Datos/Mapping/Usuarios/RolMap.cs
+++ Ucare.Datos/Mapping/Usuarios/RolMap.cs
@@ -5,7 +5,8 @@ using System.Text;
 
 namespace Ucare.Datos.Mapping.Usuarios
 {
-    public class RolMap:IEntityTypeConfiguration<Rol>
+    public class RolMap:IEntityTypeConfiguration<Rol> 
     {
+      
     }
 }
