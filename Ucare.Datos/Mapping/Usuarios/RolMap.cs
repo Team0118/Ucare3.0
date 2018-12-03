5f4b2d58f3e953107efbdc1b4e65d67b037fc480
CristoferNC <cristofercorporan675@hotmail.com>
2018-12-03 08:35:52 -0400

    Usuarios

---
+++ Ucare.Datos/Mapping/Usuarios/RolMap.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ucare.Datos.Mapping.Usuarios
+{
+    public class RolMap:IEntityTypeConfiguration<Rol>
+    {
+    }
+}
---
+++ Ucare.Entidades/Usuarios/Rol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ucare.Entidades.Usuarios
+{
+    public class Rol
+    {
+        public int idrol { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "El nombre  no debe tener más de 30 cataracteres, ni menos de 3")]
+        public string nombre { get; set; }
+        [StringLength(256)]
+        public string descripcion { get; set; }
+        public bool condicion { get; set; }
+    }
+}
